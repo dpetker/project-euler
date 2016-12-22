f76ff55f667627e8de745add1f835d32f8b935a2
Dan Petker <dan.petker@vidyard.com>
2016-12-22 14:05:25 -0500

    Problem 4

---
+++ 4.py
@@ -0,0 +1,16 @@
+# Soultion for Project Euler Problem #4 - https://projecteuler.net/problem=4
+# (c) 2016 dpetker
+
+op1 = 999
+op2 = 999
+
+curr_max = 0
+
+for i in range(999, 0, -1):
+  for j in range(999, 0, -1):
+    test = i * j
+    test_str = str(test)
+    if test_str == test_str[::-1] and test > curr_max:
+      curr_max = test
+
+print('The largest palindrome made from the product of two 3-digit numbers is {}'.format(curr_max))
