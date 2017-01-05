d0d6015b0bbf043b1d298eeb1886ca0f4eed1e2c
Dan Petker <dan.petker@vidyard.com>
2017-01-04 22:56:54 -0500

    Problem 1

--- typescript/1.ts
+++ typescript/1.ts
@@ -1,10 +1,12 @@
 // Soultion for Project Euler Problem #1 - https://projecteuler.net/problem=1
 // (c) 2017 dpetker
 
-// sum = 0
+var sum = 0;
 
-// for i in range(1, 1000):
-//   if i % 3 == 0 or i % 5 == 0:
-//     sum += i
+for (var i = 1; i < 1000; i++) {
+  if ((i % 3) === 0 || (i % 5) === 0) {
+    sum += i
+  }
+}
 
-// print('The sum of all the multiples of 3 and 5 less than 1000 is {}'.format(sum))
+console.log(`The sum of all the multiples of 3 and 5 less than 1000 is ${sum}.`);
